38bf4ace8cb5bf372160df2303a2fb06c0cd85ae
Munchie22 <145806578+Munchie22@users.noreply.github.com>
2023-11-17 02:59:42 -0600

    agrege el aviso al login

--- js/prueba.js
+++ js/prueba.js
@@ -2,6 +2,13 @@ console.log('Jake')
 
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+});
+
 document.getElementById('boton-fetch').addEventListener('click', function() {
 fetch(URL)
     .then(res => res.json())
@@ -10,3 +17,28 @@ fetch(URL)
         img.src = data[0].url;
     });
 });
+
+function mostrarMensajeBienvenida() {
+    window.alert("¡Bienvenido a nuestro sitio web!");
+}
+
+function confirmarAccion() {
+    var resultado = window.confirm("¿Estás seguro?");
+    if (resultado === true) {
+        window.alert("¡Estás seguro!");
+    } else {
+        window.alert("Pareces indeciso");
+    }
+}
+
+function solicitarInformacion() {
+    var edad = prompt("¿Cuántos años tienes?", "100");
+    if (edad !== null) {
+        window.alert("Tienes " + edad + " años.");
+    }
+}
+document.getElementById('msj-login').addEventListener('click', function(){
+    mostrarMensajeBienvenida();
+});
+
+
