ada5d4375391885076aad1580433dfcf4046759b
VicMS05 <145970377+VicMS05@users.noreply.github.com>
2023-11-20 22:58:37 -0600

    Revert "le movi al script"
    This reverts commit b943d91e25927f9776489c2fa14ca207b16912d6.

--- js/script.js
+++ js/script.js
@@ -3,24 +3,25 @@ const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 const $password = document.querySelector('input');
 const $toggler = document.querySelector('i');
+
 const showHidePassword = () => {
     if ($password.type == 'password'){
         $password.setAttribute('type','text');
     } else {
-        $password.setAttribute('type','password');
+        $password.setAttribute('type','password')
     }
 
     $toggler.classList.toggle('fa-eye');
     $toggler.classList.toggle('fa-eye-slash');    
 };
 
-$toggler.addEventListener('click', showHidePassword);
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 
 loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    container.classList.remove("active")
 });
 
+$toggler.addEventListener('click', showHidePassword);
