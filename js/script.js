1747fd40e8eafe79f0d60968bfc999215cbc2ce2
Munchie22 <145806578+Munchie22@users.noreply.github.com>
2023-11-20 18:09:40 -0600

    agregue el ocultar contraseña en login

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const $password = document.querySelector('input');
+const $toggler = document.querySelector('i');
+
+const showHidePassword = () => {
+    if ($password.type == 'password'){
+        $password.setAttribute('type','text');
+    } else {
+        $password.setAttribute('type','password')
+    }
+
+    $toggler.classList.toggle('fa-eye');
+    $toggler.classList.toggle('fa-eye-slash');    
+};
+
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,3 +23,5 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active")
 });
+
+$toggler.addEventListener('click', showHidePassword);
