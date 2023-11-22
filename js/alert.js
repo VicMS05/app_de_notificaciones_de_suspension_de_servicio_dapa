db41cd3a0a8eb055e8b0f53799835765b5f287a1
Munchie22 <145806578+Munchie22@users.noreply.github.com>
2023-11-22 14:40:24 -0600

    Le movi a las alertas y ya no jalo

--- js/alert.js
+++ js/alert.js
@@ -1,7 +1,14 @@
-const eliminarBtn = document.getElementById('btnGuardarCambiosE')
+const eliminarBtn = document.getElementById('btnGuardarCambiosE');
+const agregarBtn = docuemnt.getElementById('btnGuardarCambiosA');
 
 eliminarBtn.addEventListener('click', () => {
     Swal.fire({
-        title: "Eliminado Correctamente",
+        title: "Eliminado Correctamente"
     });
-})
+});
+
+// agregarBtn.addEventListener('click', () => {
+//     Swal.fire({
+//         title: "Agregado Correctamente"
+//     });
+// })
